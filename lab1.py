d424472a5395ea841d7a8b69231343df59f6e2f1
EduardoCFonte <148607226+EduardoCFonte@users.noreply.github.com>
2024-10-02 19:22:58 -0300

    Improving my function

--- lab1.py
+++ lab1.py
@@ -1,9 +1,11 @@
 import time
 import os
 
-def shell_sort(arr, file):
-    n = len(arr)
-    gap = n // 2 # Inicializa o "gap", metade do tamanho da lista
+def shell_sort(arr, file,sequencia,n):
+
+    if not sequencia:
+        gap = n // 2 # Inicializa o "gap", metade do tamanho da lista
+    
 
     # Diminui o gap até que ele seja 0
     while gap > 0:
@@ -25,21 +27,41 @@ def shell_sort(arr, file):
 
 
 path = "entrada1.txt"
+path2 = "entrada2.txt"
+ciura = [701,301,132,57,23,10,4,1]
 
 try:
-    with open (path, ) as file:
+    with open (path) as file:
         out_file =  open("saida1.txt", "w")
         sequencia = file.readline()
         while sequencia:
             sequencia = sequencia.split()
-            shell_sort(sequencia,out_file)
+            numeros_list = [int(num) for num in sequencia]
+            tamanho = numeros_list.pop(0)
+            out_file.write("Ordenando agora uma lista com {} numeros \n".format(tamanho))
+            shell_sort(numeros_list,out_file,0,tamanho)
             out_file.write("\n")
             sequencia = file.readline()
         
         out_file.close()
-        
+
 except FileNotFoundError:
     print("Não achamos o file")
+
+# try:
+#     with open (path2) as file:
+#         out_file =  open("saida2.txt", "w")
+#         sequencia = file.readline()
+#         while sequencia:
+#             sequencia = sequencia.split()
+#             shell_sort(sequencia,out_file)
+#             out_file.write("\n")
+#             sequencia = file.readline()
+        
+#         out_file.close()
+
+# except FileNotFoundError:
+#     print("Não achamos o file")
 
 
 
